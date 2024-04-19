2c36396ef7b7d0e12b3754760221cfabf8be93ac
mannumishra <mannumishra2207@outlook.com>
2024-04-19 11:42:00 +0530

    add

--- Routes/UserRouter.js
+++ Routes/UserRouter.js
@@ -1,8 +1,9 @@
 const { newRegister, login, getByUserId } = require("../Controllar/UserControllar")
+const { verifyAdmin } = require("../verification")
 
 const UserRouter = require("express").Router()
 
-UserRouter.post("/", newRegister)
+UserRouter.post("/",verifyAdmin, newRegister)
 UserRouter.post("/login", login)
 UserRouter.get("/:_id" ,getByUserId)
 
