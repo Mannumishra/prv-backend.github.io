f060594bddde0f036f1c621c828c09a40c9e2e7f
mannumishra <mannumishra2207@outlook.com>
2024-04-19 13:57:13 +0530

    aad

--- Controllar/OrderControllar.js
+++ Controllar/OrderControllar.js
@@ -5,7 +5,6 @@ exports.createRecord = async (req, res) => {
     try {
         const { userid, cartItems } = req.body
         console.log("i am hittttt");
-        // console.log(req.body,"order")
         if (!userid || !cartItems) {
             return res.status(400).json({
                 success: false,
@@ -25,18 +24,13 @@ exports.createRecord = async (req, res) => {
 }
 exports.getRecord = async (req, res) => {
     try {
-        // Fetch orders and populate the userid field with user details
         let orders = await Order.find().populate('userid');
 
         let combinedData = [];
-
-        // Loop through each order
         for (let order of orders) {
-            // Find user details for the order
             let user = await User.findById(order.userid);
 
             if (user) {
-                // Combine order details with user details
                 let combinedOrder = {
                     orderId: order._id,
                     product: order.product,
@@ -44,24 +38,18 @@ exports.getRecord = async (req, res) => {
                     user: {
                         userId: user._id,
                         username: user.name,
-                        phone:user.phone,
+                        phone: user.phone,
                         email: user.email
-                        // Include other user details as needed
                     }
                 };
-
-                // Push combined order details to the array
                 combinedData.push(combinedOrder);
             }
         }
-
-        // Send the response with the combined data
         res.status(200).json({
             success: true,
             data: combinedData
         });
     } catch (error) {
-        // If an error occurs, log it and send an error response
         console.log(error);
         res.status(500).json({
             success: false,
